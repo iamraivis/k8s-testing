657df302e6cea97880cda2a90289977ec77b8db7
iamraivis <rozols8@gmail.com>
2026-03-29 19:46:43 +0300

    deploying broken version

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException
 from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI()
-
-# This single line instruments the app and exposes the /metrics endpoint
 Instrumentator().instrument(app).expose(app)
+
+# Chaos Variable
+IS_BROKEN = os.getenv("IS_BROKEN", "false").lower() == "true"
 
 @app.get("/")
 def read_root():
-    return {"Hello": "GitOps World"}
+    if IS_BROKEN:
+        # Simulate a server crash / error
+        raise HTTPException(status_code=500, detail="Chaos Monkey hit the server!")
+    return {"status": "healthy", "version": "v3-stable"}
